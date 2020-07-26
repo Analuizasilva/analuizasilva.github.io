f69a3503d43fdb6f53891ebc2d12d36018121988
Analuizasilva <a.silva@recodme.pt>
2020-07-26 22:44:22 +0100

    add javascript

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,7 @@ const idade = (day, month, year) => {
   const today = new Date();
   debugger;
   console.log(today.getMonth());
-  console.log(today.());
+  console.log(today.getUTCDay());
   if (today.getMonth() > month) {
     return today.getFullYear() - year;
   } else {
@@ -18,4 +18,4 @@ h2About.innerText = `Sou Ana Luiza Silva, brasileira e natural do estado de Mina
   24,
   07,
   1996
-)} anos, e atualmente vivo em Lisboa. Formada em Tecnico em automação industrial pelo instituto Federal do Espirito Santo, e sem experiencia profissional na area, decidi recomeçar minha carreira profissional e me dedicar a uma vontade pessoal pela area da programação `;
+)} anos, e atualmente vivo em Lisboa. Formada em Tecnico em automação industrial pelo instituto Federal do Espirito Santo, e sem experiencia profissional na area, decidi recomeçar minha carreira profissional e me dedicar a uma vontade pessoal pela area da programação. Atualmente enconto-me em formação em contexto de trabalho pelo curso recodme de programação em C#. `;
