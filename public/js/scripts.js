21bc41a98951f370b267c4e88d0f458fd51a7857
Analuizasilva <a.silva@recodme.pt>
2020-07-26 16:15:55 +0100

    add abou me

---
+++ public/js/scripts.js
@@ -0,0 +1,21 @@
+const idade = (day, month, year) => {
+  const today = new Date();
+  debugger;
+  console.log(today.getMonth());
+  console.log(today.());
+  if (today.getMonth() > month) {
+    return today.getFullYear() - year;
+  } else {
+    if (today.getDay() >= day && today.getMonth() >= month)
+      return today.getFullYear() - year;
+    else return today.getFullYear() - year - 1;
+  }
+};
+
+const h2About = document.getElementById("h2-about");
+
+h2About.innerText = `Sou Ana Luiza Silva, brasileira e natural do estado de Minas Gerais. Tenho ${idade(
+  24,
+  07,
+  1996
+)} anos, e atualmente vivo em Lisboa. Formada em Tecnico em automação industrial pelo instituto Federal do Espirito Santo, e sem experiencia profissional na area, decidi recomeçar minha carreira profissional e me dedicar a uma vontade pessoal pela area da programação `;
