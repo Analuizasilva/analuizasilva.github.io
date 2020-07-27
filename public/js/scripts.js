0bfa597236805c797fd89ced54867d039df0278e
Analuizasilva <a.silva@recodme.pt>
2020-07-27 11:56:57 +0100

    about responsive

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,4 +18,4 @@ h2About.innerText = `I'm Ana Luiza Silva, Brazilian and from the state of Minas 
   24,
   07,
   1996
-)}years old and currently live in Lisbon. Graduated in Industrial Automation Technician from the Federal Institute of Espirito Santo, and without professional experience in the area, I decided to start my professional career again and dedicate myself to a personal will for the area of ​​programming. I am currently in training in a work context through the C # programming course. `;
+)} years old and currently live in Lisbon. Graduated in Industrial Automation Technician from the Federal Institute of Espirito Santo, and without professional experience in the area, I decided to start my professional career again and dedicate myself to a personal will for the area of ​​programming. I am currently in training in a work context through the C # programming course. `;
